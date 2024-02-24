8c30adec92c24d8c108b081750f30f04f14d2530
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2024-02-24 12:16:06 +0100

    setting direction based on movement

--- dome-defender/Assets/Scripts/PlayerController.cs
+++ dome-defender/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,30 @@ public class PlayerController : MonoBehaviour
     private float _speed = 5.0f;
 
     private Rigidbody2D _rigidbody;
+    private Transform _drill;
 
     private Vector3 _movement;
+    private Vector2 _direction;
 
     void Start() {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _drill = transform.Find("Drill");
     }
 
     public void FixedUpdate() {
         Vector3 delta = _speed * Time.fixedDeltaTime * _movement;
         _rigidbody.MovePosition(transform.position + delta);
+
+        if (!_direction.Equals(Vector2.zero)) { 
+            float radian = Mathf.Atan2(_direction.y, _direction.x);
+            float degree = radian * Mathf.Rad2Deg;
+            float z = degree - 90;
+            _drill.rotation = Quaternion.Euler(0, 0, z);
+        }
     }
 
     public void OnMovement(InputValue value) {
         _movement = value.Get<Vector2>();
+        _direction = _movement.normalized;
     }
 }
